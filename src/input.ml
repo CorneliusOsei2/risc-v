68b4527902f826dcee418ab71d9aa89ef19310db
Shreya Kumar <sk2329@cornell.edu>
2022-10-15 23:26:49 -0400

    Implemented list_to_file function

--- src/input.ml
+++ src/input.ml
@@ -10,3 +10,14 @@ let file_to_list file =
   with End_of_file ->
     close_in ic;
     List.rev !list
+
+let rec print_lst oc = function
+  | [] -> ()
+  | h :: t ->
+      Printf.fprintf oc "%s\n" h;
+      print_lst oc t
+
+let list_to_txt lst =
+  let oc = open_out "instructions.txt" in
+  print_lst oc lst;
+  close_out oc
--- src/input.mli
+++ src/input.mli
@@ -1,3 +1,7 @@
 val file_to_list : string -> string list
 (*[file_to_list] takes a txt file with individual instructions and returns
    a string list with each element being one instruction.contents *)
+
+val list_to_file : string list -> unit
+(*[list_to_file] takes a list with each element being an instruction
+   and returns a txt file with each line being an instruction *)
