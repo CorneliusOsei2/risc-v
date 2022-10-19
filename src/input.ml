c710b52ab936ac6c249630c3e76f3239df639633
Shreya Kumar <sk2329@cornell.edu>
2022-10-19 12:31:46 -0400

    Updated, recursive version of file_to_list

--- src/input.ml
+++ src/input.ml
@@ -1,15 +1,13 @@
 let file_to_list file =
-  let list = ref [] in
   let ic = open_in file in
-  try
-    while true do
-      let input = input_line ic in
-      list := input :: !list
-    done;
-    !list
-  with End_of_file ->
-    close_in ic;
-    List.rev !list
+  let rec loop acc =
+    match try Some (input_line ic) with End_of_file -> None with
+    | Some a -> loop (a :: acc)
+    | None ->
+        close_in ic;
+        List.rev acc
+  in
+  loop []
 
 let rec print_lst oc = function
   | [] -> ()
