84bb946e5ff8ff648dee5f5c2e52faf59eaf0c78
CorneliusOsei2 <oseiowusuboatengcornelius6@gmail.com>
2022-10-21 12:43:25 -0400

    Incorporated input insn visualization pattern

--- bin/system.ml
+++ bin/system.ml
@@ -6,13 +6,32 @@ open IO
 open ProcessInstructions
 
 let data_dir_prefix = "data" ^ Filename.dir_sep
-let process f = process_input_insns (file_to_list f)
+let ansi_print color = ANSITerminal.print_string [ color ]
+
+let eval_pattern pat f =
+  match String.trim pat with
+  | "run all" | "r" -> process_input_insns (file_to_list f)
+  | "step" -> ()
+  | _ -> ()
+
+let eval_pattern_inpt f =
+  ansi_print ANSITerminal.green "\n File successfully loaded!\n";
+  ansi_print ANSITerminal.blue
+    "\n\
+     How will you like to visualize the execution? \n\
+    \ step (s) or run all (r)? \n";
+  ansi_print ANSITerminal.yellow ">> ";
+  match read_line () with
+  | exception End_of_file -> ()
+  | pattern -> eval_pattern pattern f
+
+let process f = eval_pattern_inpt f
 
 let main () =
-  ANSITerminal.print_string [ ANSITerminal.red ]
-    "\n\nWelcome to The RISC-V Processor.\n";
-  print_endline "Please enter the name of the test file you want to load.\n";
-  print_string ">> ";
+  ansi_print ANSITerminal.red "\n\nWelcome to The RISC-V Processor.\n";
+  ansi_print ANSITerminal.blue
+    "Please enter the name of the test file you want to load.\n";
+  ansi_print ANSITerminal.yellow ">> ";
   match read_line () with
   | exception End_of_file -> ()
   | file_name -> process (data_dir_prefix ^ file_name ^ ".txt")
