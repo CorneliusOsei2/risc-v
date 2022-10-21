35a2931bb02aabe1f0cdf113dfc7cb86983779fb
Shreya Kumar <sk2329@cornell.edu>
2022-10-21 00:30:25 -0400

    Implemented input from txt conversions.

--- test/main.ml
+++ test/main.ml
@@ -2,25 +2,10 @@ open OUnit2
 open Processor
 open Registers
 open Utilities
-open Input
+open IO
 open ProcessInstructions
 
-let rfile = register_init
-let rfile1 = update_register "x1" 5 rfile
-
-(** [test_register name adv expected_output] constructs an OUnit test named
-    [name] that asserts the quality of [expected_output] with
-    [Registers.get_register r rfile]. *)
-let test_register (name : string) (r : string)
-    (rfile : (int * bool) RegisterFile.t) (expected_output : int) : test =
-  name >:: fun _ ->
-  assert_equal (get_register "x1" rfile) expected_output ~printer:string_of_int
-
-let register_tests =
-  [
-    test_register "init values" "x1" rfile 0;
-    test_register "update register" "x1" rfile1 5;
-  ]
+(************************************ Utilities Tests *********************************** *)
 
 (*[test_dec_to_bin] compares the [dec_to_bin n] to expected output*)
 let test_dec_to_bin (name : string) (n : int) (expected_output : string) : test
@@ -43,6 +28,42 @@ let dec_conversions_tests =
     test_dec_to_hex "" 45 "0x0000002d";
   ]
 
-let suite = "test suite for A2" >::: List.flatten [ dec_conversions_tests; register_tests  ]
-let _ = run_test_tt_main suite
+(************************************ IO Tests *********************************** *)
 
+(** [test_file_to_list name file expected_output] constructs an OUnit test named
+    [name] that asserts the quality of [expected_output] with
+    [file_to_list file]. *)
+let test_file_to_list (name : string) (file : string)
+    (expected_output : string list) : test =
+  name >:: fun _ -> assert_equal expected_output (file_to_list file)
+
+let data_dir_prefix = "data" ^ Filename.dir_sep
+let e1 = [ "addi x1, x1, 6"; "addi x1, x1, 6"; "addi x1, x1, 6" ]
+
+let file_to_list_tests =
+  [ test_file_to_list "" (data_dir_prefix ^ "test" ^ ".txt") [] ]
+
+(************************************ Registers Tests *********************************** *)
+
+let rfile = register_init
+let rfile1 = update_register "x1" 5 rfile
+
+(** [test_register name r expected_output] constructs an OUnit test named
+    [name] that asserts the quality of [expected_output] with
+    [Registers.get_register r rfile]. *)
+let test_register (name : string) (r : string)
+    (rfile : (int * bool) RegisterFile.t) (expected_output : int) : test =
+  name >:: fun _ ->
+  assert_equal (get_register "x1" rfile) expected_output ~printer:string_of_int
+
+let register_tests =
+  [
+    test_register "init values" "x1" rfile 0;
+    test_register "update register" "x1" rfile1 5;
+  ]
+
+let suite =
+  "test suite for A2"
+  >::: List.flatten [ dec_conversions_tests; register_tests ]
+
+let _ = run_test_tt_main suite
