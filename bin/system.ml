16c46df85b13e318043da61af1520e27044c82f0
CorneliusOsei2 <oseiowusuboatengcornelius6@gmail.com>
2022-10-21 13:05:58 -0400

    Incorporated input insn visualization pattern

--- bin/system.ml
+++ bin/system.ml
@@ -8,27 +8,34 @@ open ProcessInstructions
 let data_dir_prefix = "data" ^ Filename.dir_sep
 let ansi_print color = ANSITerminal.print_string [ color ]
 
-let eval_pattern pat f =
-  match String.trim pat with
-  | "run all" | "r" -> process_input_insns (file_to_list f)
-  | "step" -> ()
-  | _ -> ()
-
-let eval_pattern_inpt f =
-  ansi_print ANSITerminal.green "\n File successfully loaded!\n";
-  ansi_print ANSITerminal.blue
-    "\n\
-     How will you like to visualize the execution? \n\
-    \ step (s) or run all (r)? \n";
+let rec eval_pattern f =
   ansi_print ANSITerminal.yellow ">> ";
   match read_line () with
   | exception End_of_file -> ()
-  | pattern -> eval_pattern pattern f
+  | pat -> (
+      match String.trim pat with
+      | "run all" | "r" -> process_input_insns (file_to_list f)
+      | "step" -> ()
+      | "q" | "quit" -> print_string "Hope you had fun! 😃 Bye! 👋👋🏽\n"
+      | _ ->
+          ansi_print ANSITerminal.red "Invalid option. Please try again: \n";
+          eval_pattern f)
+
+let eval_pattern_inpt f =
+  ansi_print ANSITerminal.green "\n .....file successfully loaded!\n";
+  ansi_print ANSITerminal.blue
+    "\n\
+     How will you like to visualize the execution? \n\
+     You can hit [r] in the process of stepping to evaluate all!\n";
+  print_endline "\t step (s) or run all (r)?";
+  eval_pattern f
 
 let process f = eval_pattern_inpt f
 
 let main () =
   ansi_print ANSITerminal.red "\n\nWelcome to The RISC-V Processor.\n";
+  ansi_print ANSITerminal.yellow
+    "You can quit at any time with [q] or [quit]\n\n";
   ansi_print ANSITerminal.blue
     "Please enter the name of the test file you want to load.\n";
   ansi_print ANSITerminal.yellow ">> ";
