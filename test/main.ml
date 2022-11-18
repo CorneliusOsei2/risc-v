cab6c69a05c7ce2a77015f7945113797c6d7c7ae
Alice Ke <aliceke64@gmail.com>
2022-11-18 14:34:54 -0500

    Restructured test file

--- test/main.ml
+++ test/main.ml
@@ -10,42 +10,18 @@ open Int32
 
 (************************************ Utilities Tests *********************************** *)
 
-(*[test_dec_to_bin name n expected_output] constructs an OUnit test named [name]
-    that asserts the quality of expected output with [dec_to_bin n]*)
-let test_dec_to_bin (name : string) (n : int) (expected_output : string) : test
-    =
-  name >:: fun _ ->
-  assert_equal expected_output (dec_to_bin n) ~printer:String.escaped
-
-(*[test_dec_to_hex name n expected_output] constructs an OUnit test named [name]
-    that asserts the quality of expected output with [dec_to_hex n]*)
-let test_dec_to_hex (name : string) (n : int) (expected_output : string) : test
-    =
-  name >:: fun _ ->
-  assert_equal expected_output (dec_to_hex n) ~printer:String.escaped
-
-(*[test_split_instruction name n expected_output] constructs an OUnit test
-    named [name] that asserts the quality of expected output with [split_instruction n]*)
-let test_split_instruction (name : string) (n : string)
-    (expected_output : string * string list) : test =
-  name >:: fun _ ->
-  assert_equal expected_output (split_instruction n) ~printer:pp_instruction
-
-let test_split_stype (name : string) (n : string)
-    (expected_output : string * string list) : test =
-  name >:: fun _ ->
-  assert_equal expected_output (split_stype n) ~printer:pp_instruction
-
 module UtilityTests = struct
-  let dec_conversions_tests =
-    [
-      test_dec_to_bin "convert 10 to binary rep" 10
-        "0b00000000000000000000000000001010";
-      test_dec_to_bin "convert 56 to binary" 56
-        "0b00000000000000000000000000111000";
-      test_dec_to_hex "convert 26 to binary" 26 "0x0000001a";
-      test_dec_to_hex "convert 45 to binary" 45 "0x0000002d";
-    ]
+  (*[test_split_instruction name n expected_output] constructs an OUnit test
+      named [name] that asserts the quality of expected output with [split_instruction n]*)
+  let test_split_instruction (name : string) (n : string)
+      (expected_output : string * string list) : test =
+    name >:: fun _ ->
+    assert_equal expected_output (split_instruction n) ~printer:pp_instruction
+
+  let test_split_stype (name : string) (n : string)
+      (expected_output : string * string list) : test =
+    name >:: fun _ ->
+    assert_equal expected_output (split_stype n) ~printer:pp_instruction
 
   let split_instruction_tests =
     [
@@ -80,14 +56,14 @@ end
 
 (************************************ IO Tests *********************************** *)
 
-(** [test_file_to_list name file expected_output] constructs an OUnit test named
+module IOTests = struct
+  (** [test_file_to_list name file expected_output] constructs an OUnit test named
     [name] that asserts the quality of [expected_output] with
     [file_to_list file]. *)
-let test_file_to_list (name : string) (file : string)
-    (expected_output : string list) : test =
-  name >:: fun _ -> assert_equal expected_output (file_to_list file)
+  let test_file_to_list (name : string) (file : string)
+      (expected_output : string list) : test =
+    name >:: fun _ -> assert_equal expected_output (file_to_list file)
 
-module IOTests = struct
   let data_dir_prefix = "data" ^ Filename.dir_sep
   let e1 = [ "addi x1, x1, 6"; "addi x1, x1, 6"; "addi x1, x1, 6" ]
   let file_to_list_tests = []
@@ -95,27 +71,27 @@ module IOTests = struct
 end
 (************************************ Registers Tests *********************************** *)
 
-let rfile = register_init
-let rfile1 = update_register "x1" 5l rfile
-let rfile2 = update_register "x2" 7l rfile
-let rfile3 = update_register "x3" 56l rfile
-let rfile4 = update_register "x4" 112l rfile
-let rfile5 = update_register "x5" 320l rfile
-let rfile6 = update_register "x6" 97l rfile
-let lower_bound = (pow 2 31 - 1) * -1 |> of_int
-let upper_bound = pow 2 31 - 1 |> of_int
-let rfile7 = update_register "x7" lower_bound rfile
-let rfile8 = update_register "x8" upper_bound rfile
+module RegisterTests = struct
+  let rfile = register_init
+  let rfile1 = update_register "x1" 5l rfile
+  let rfile2 = update_register "x2" 7l rfile
+  let rfile3 = update_register "x3" 56l rfile
+  let rfile4 = update_register "x4" 112l rfile
+  let rfile5 = update_register "x5" 320l rfile
+  let rfile6 = update_register "x6" 97l rfile
+  let lower_bound = (pow 2 31 - 1) * -1 |> of_int
+  let upper_bound = pow 2 31 - 1 |> of_int
+  let rfile7 = update_register "x7" lower_bound rfile
+  let rfile8 = update_register "x8" upper_bound rfile
 
-(** [test_register name r expected_output] constructs an OUnit test named
+  (** [test_register name r expected_output] constructs an OUnit test named
     [name] that asserts the quality of [expected_output] with
     [Registers.get_register r rfile]. *)
-let test_register (name : string) (r : string)
-    (rfile : (int32 * bool) RegisterFile.t) (expected_output : int32) : test =
-  name >:: fun _ ->
-  assert_equal expected_output (get_register r rfile) ~printer:Int32.to_string
+  let test_register (name : string) (r : string)
+      (rfile : (int32 * bool) RegisterFile.t) (expected_output : int32) : test =
+    name >:: fun _ ->
+    assert_equal expected_output (get_register r rfile) ~printer:Int32.to_string
 
-module RegisterTests = struct
   let register_tests =
     [
       test_register "init values" "x1" rfile 0l;
@@ -131,20 +107,20 @@ module RegisterTests = struct
 end
 (************************************ Memory Tests *********************************** *)
 
-let mem = memory_init
-let mem1 = update_memory 0 5 mem
-let mem2 = update_memory 1 2 mem
-let mem3 = update_memory 15 5 mem
+module MemoryTests = struct
+  let mem = memory_init
+  let mem1 = update_memory 0 5 mem
+  let mem2 = update_memory 1 2 mem
+  let mem3 = update_memory 15 5 mem
 
-(** [test_memory name addr expected_output] constructs an OUnit test named
+  (** [test_memory name addr expected_output] constructs an OUnit test named
     [name] that asserts the quality of [expected_output] with
     [Memory.get_memory addr mem]. *)
-let test_memory (name : string) (addr : int) (mem : (int * bool) Memory.t)
-    (expected_output : int) : test =
-  name >:: fun _ ->
-  assert_equal (get_memory addr mem) expected_output ~printer:string_of_int
+  let test_memory (name : string) (addr : int) (mem : (int * bool) Memory.t)
+      (expected_output : int) : test =
+    name >:: fun _ ->
+    assert_equal (get_memory addr mem) expected_output ~printer:string_of_int
 
-module MemoryTests = struct
   let memory_tests =
     let _ = GenerateInstructions.gen_itype "subi" 15 [] in
     [
@@ -160,7 +136,6 @@ let suite =
   "test suite for A2"
   >::: List.flatten
          [
-           UtilityTests.dec_conversions_tests;
            UtilityTests.split_instruction_tests;
            UtilityTests.split_stype_tests;
            IOTests.file_to_list_tests;
