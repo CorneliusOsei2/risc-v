46459346688a8e6915bf6f2593888044fc1e92dc
Shreya Kumar <sk2329@cornell.edu>
2022-12-14 00:32:29 -0800

    Done with op types

--- test/main.ml
+++ test/main.ml
@@ -117,6 +117,7 @@ module RegisterTests = struct
       test_register "upper bound" "x8" rfile8 upper_bound;
     ]
 end
+
 (************************************ Memory Tests *********************************** *)
 (*
    let mem = memory_init
@@ -124,13 +125,17 @@ end
    let mem2 = update_memory 1 2 mem
    let mem3 = update_memory 15 5 mem
 
-   (** [test_memory name addr expected_output] constructs an OUnit test named
+   [test_memory name addr expected_output] constructs an OUnit test named
        [name] that asserts the quality of [expected_output] with
-       [Memory.get_memory addr mem]. *)
-   let test_memory (name : string) (addr : int) (mem : (int * bool) Memory.t)
-       (expected_output : int) : test =
-     name >:: fun _ ->
-     assert_equal (get_memory addr mem) expected_output ~printer:string_of_int
+       [Memory.get_memory addr mem]. 
+module MemoryTests = struct
+  let test_memory (name : string) (addr : int)
+      (mem : ('t * bool) Memory.Memory.t) (expected_output : int) : test =
+    name >:: fun _ ->
+    assert_equal
+      (Memory.get_memory addr mem)
+      expected_output ~printer:string_of_int
+end
 
    module MemoryTests = struct
      let memory_tests =
@@ -150,8 +155,24 @@ let _ = Memory.(init |> pp_memory)
 module ProcessInstructionsTests = struct
   include RegisterTests
 
+  let test_register_stype (name : string) (r : string)
+      (reg_mem : (int32 * bool) RegisterFile.t * (int32 * bool) Memory.Memory.t)
+      (expected_output : int32) : test =
+    name >:: fun _ ->
+    assert_equal
+      (get_register r (fst reg_mem))
+      expected_output ~printer:Int32.to_string
+
+  let test_memory_stype (name : string) (addr : int)
+      (reg_mem : (int32 * bool) RegisterFile.t * (int32 * bool) Memory.Memory.t)
+      (expected_output : int32) : test =
+    name >:: fun _ ->
+    assert_equal
+      (Memory.get_memory addr (snd reg_mem))
+      expected_output ~printer:Int32.to_string
+
   let rfile = Registers.init
-  let rfile1 = update_register "x1" 9 rfile
+  let rfile1 = process_itype "addi" "x1" "x1" "9" rfile
   let rfile2 = process_itype "addi" "x2" "x1" "10" rfile1
   let rfile3 = process_rtype "add" "x3" "x1" "x2" rfile2
   let rfile4 = process_itype "addi" "x4" "x4" "2047" rfile3
@@ -171,7 +192,14 @@ module ProcessInstructionsTests = struct
   let rfile18 = process_itype "ori" "x18" "x16" "48" rfile17
   let rfile19 = process_itype "xori" "x19" "x18" "174" rfile18
   let rfile20 = process_itype "addi" "x20" "x20" "-2048" rfile19
-  (* let rfile21 = process_itype "slli" "x21" "x7" "101" rfile20 *)
+  let rfile21 = process_itype "addi" "x21" "x21" "256" rfile20
+  let rfile22 = process_itype "addi" "x23" "x23" "14" rfile20
+  let mem = Memory.init
+  let reg_mem1 = process_stype "sw" "x18" "12" "x17" rfile20 mem
+  let reg_mem2 = process_stype "sb" "x21" "12" "x1" rfile21 (snd reg_mem1)
+  let reg_mem3 = process_stype "lw" "x22" "5" "x19" rfile21 (snd reg_mem2)
+  let reg_mem4 = process_stype "sb" "x9" "2" "x23" rfile22 (snd reg_mem2)
+  let reg_mem5 = process_stype "lb" "x24" "5" "x2" rfile21 (snd reg_mem3)
 
   let process_optype_tests =
     [
@@ -189,12 +217,12 @@ module ProcessInstructionsTests = struct
          register file"
         "x1" rfile2 9l;
       test_register
-        "edge case where we test that the maximum integer that addi operations \
-         can represent is 2^11 - 1 "
+        "edge case where we test that the maximum integer that an itype \
+         operations can represent is 2^11 - 1 "
         "x4" rfile4 2047l;
       test_register
-        "edge case where we test that the minimum integer the addi operations \
-         can represent is -2^11 "
+        "edge case where we test that the minimum integer that an itype \
+         operation can represent is -2^11 "
         "x20" rfile20 (-2048l);
       test_register "non-edge case of the add operation" "x3" rfile3 28l;
       test_register "non-edge case of the sub operation" "x5" rfile5 19l;
@@ -213,8 +241,40 @@ module ProcessInstructionsTests = struct
         "x15" rfile15 0l;
       test_register "non-edge case of the andi operation" "x17" rfile17 12l;
       test_register "non-edge case of the ori operation" "x18" rfile18 316l;
-      test_register "non-edge case of the xori operation" "x19" rfile19 402l
-      (* test_register "non-edge case of the slli operation" "x21" rfile21 11l; *);
+      test_register "non-edge case of the xori operation" "x19" rfile19 402l;
+      test_memory_stype
+        "non-edge case of sw operation in which we check the updated value at \
+         the memory address"
+        24 reg_mem1 316l;
+      test_register_stype
+        "non-edge case of sw operation in which we check the value of the \
+         register destination  which should be unchanged"
+        "x18" reg_mem1 316l;
+      test_memory_stype
+        "edge case of store operation in which we check that the max \
+         representation of store byte is 2^8 - 1 since it wraps values greater \
+         than this"
+        21 reg_mem2 0l;
+      test_register_stype
+        "non-edge case of lw operation in which we check the value of the \
+         register destination  which should be changed"
+        "x22" reg_mem3 60l;
+      test_memory_stype
+        "non-edge case of lw operation in which we check the value at the \
+         memory address which should be unchanged"
+        24 reg_mem3 316l;
+      test_register_stype
+        "non-edge case of lb operation in which we check the value of the \
+         register destination  which should be changed"
+        "x24" reg_mem5 60l;
+      test_memory_stype
+        "non-edge case of sb operation in which we check the value of the \
+         memory address  which should be changed"
+        16 reg_mem4 11l;
+      test_register_stype
+        "non-edge case of sb operation in which we check the value of the \
+         memory address  which should be changed"
+        "x9" reg_mem4 11l;
     ]
 end
 
